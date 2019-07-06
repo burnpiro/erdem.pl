a47db7f909b6e176073e337f093f647a75108e7f
burnpiro <kemalpiro@gmail.com>
2019-07-06 22:38:17 +0200

    Implement time to read component to display coffees :)

---
+++ src/components/Post/ReadTime/ReadTime.js
@@ -0,0 +1,35 @@
+// @flow
+import React from 'react';
+import styles from './ReadTime.module.scss';
+
+import pikaCoffee from './coffee-pika.svg';
+import coffee from './coffee.svg';
+
+type Props = {
+  readTime: {
+    text: string,
+    minutes: number,
+  },
+};
+
+const ReadTime = ({ readTime: { text, minutes } }: Props) => (
+  <span className={styles['readTime']}>
+    {Array.from(new Array(Math.floor(Number(minutes / 20)))).map(
+      (key, index) => (
+        <img
+          src={pikaCoffee}
+          key={index}
+          className={styles['readTime__icon-pika']}
+        />
+      )
+    )}
+    {Array.from(
+      new Array((Math.floor(minutes / 5) % 4) + (minutes < 20 ? 1 : 0))
+    ).map((key, index) => (
+      <img src={coffee} key={index} className={styles['readTime__icon']} />
+    ))}
+    <span className={styles['readTime__text']}>{text}</span>
+  </span>
+);
+
+export default ReadTime;
