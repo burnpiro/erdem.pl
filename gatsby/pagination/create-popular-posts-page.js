029e2af5696787419cc3e8cea45d5052ba351108
Kemal Erdem <kemalpiro@gmail.com>
2023-10-18 10:58:36 +0200

    Include all posts in popular

--- gatsby/pagination/create-popular-posts-page.js
+++ gatsby/pagination/create-popular-posts-page.js
@@ -2,13 +2,13 @@ const path = require('path');
 const siteConfig = require('../../config.js');
 
 module.exports = async (graphql, actions) => {
-    const { createPage } = actions;
+  const { createPage } = actions;
 
-    const result = await graphql(`
+  const result = await graphql(`
     {
       allMarkdownRemark(
         filter: {
-            frontmatter: { draft: { eq: false }, popular: { ne: null }, template: { eq: "post" } }
+          frontmatter: { draft: { eq: false }, template: { eq: "post" } }
         }
       ) {
         totalCount
@@ -16,24 +16,24 @@ module.exports = async (graphql, actions) => {
     }
   `);
 
-    const { postsPerPage } = siteConfig;
-    const numPages = Math.ceil(
-        result.data.allMarkdownRemark.totalCount / postsPerPage
-    );
+  const { postsPerPage } = siteConfig;
+  const numPages = Math.ceil(
+    result.data.allMarkdownRemark.totalCount / postsPerPage
+  );
 
-    for (let i = 0; i < numPages; i += 1) {
-        createPage({
-            path: i === 0 ? '/' : `/popular/page/${i}`,
-            component: path.resolve('./src/templates/popular-template.js'),
-            context: {
-                currentPage: i,
-                postsLimit: postsPerPage,
-                postsOffset: i * postsPerPage,
-                prevPagePath: i <= 1 ? '/' : `/popular/page/${i - 1}`,
-                nextPagePath: `/popular/page/${i + 1}`,
-                hasPrevPage: i !== 0,
-                hasNextPage: i !== numPages - 1,
-            },
-        });
-    }
+  for (let i = 0; i < numPages; i += 1) {
+    createPage({
+      path: i === 0 ? '/' : `/popular/page/${i}`,
+      component: path.resolve('./src/templates/popular-template.js'),
+      context: {
+        currentPage: i,
+        postsLimit: postsPerPage,
+        postsOffset: i * postsPerPage,
+        prevPagePath: i <= 1 ? '/' : `/popular/page/${i - 1}`,
+        nextPagePath: `/popular/page/${i + 1}`,
+        hasPrevPage: i !== 0,
+        hasNextPage: i !== numPages - 1,
+      },
+    });
+  }
 };
