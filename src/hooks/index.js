c24ce894159bc22ed8831f32282ba8aed1ccc380
Kemal Erdem <kemalpiro@gmail.com>
2023-10-25 20:26:51 +0200

    Create fetcher for diagram definitions

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,5 @@
 // @flow
 export { default as useSiteMetadata } from './use-site-metadata';
 export { default as useTagsList } from './use-tags-list';
+export { default as useLoadDiagramData } from './use-load-diagram-data';
+export { default as useWindowSize } from './use-window-size';
